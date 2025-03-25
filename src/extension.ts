6b2233eb6115562d448f2b70ed91582846c95876
Jansen Lin <jiansen@ai-menu.com>
2025-03-25 19:53:09 +1000

    attempting to develop signatureHelpProvider

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import { b4xDefinitionProvider, KeywordInfo, KeywordScope, ModuleType } from './b4xDefinitionProvider';
 import { b4xHoverProvider } from './b4xHoverProvider';
 import { b4xReferenceProvider } from './b4xReferenceProvider';
-import { B4XCompletionItemProvider } from './b4xCompletionItemProvider';
+import { B4XCompletionItemProvider as b4xCompletionItemProvider } from './b4xCompletionItemProvider';
 
 export function activate(context: vscode.ExtensionContext) 
 {
@@ -21,9 +21,11 @@ export function activate(context: vscode.ExtensionContext)
     context.subscriptions.push(vscode.languages.registerReferenceProvider('b4x', referenceProvider));
     //const t: boolean = true;
 
-    const provider = new B4XCompletionItemProvider();
-    context.subscriptions.push(vscode.languages.registerCompletionItemProvider('b4x', provider));
+    const completionItemProvider = new b4xCompletionItemProvider();
+    context.subscriptions.push(vscode.languages.registerCompletionItemProvider('b4x', completionItemProvider));
 
+    const signatureHelpProvider = new b4xSignatureHelpProvider();
+    context.subscriptions.push(vscode.languages.registerSignatureHelpProvider('b4x', signatureHelpProvider,'(', ','));
     // // Listen for when a .bas file is opened
     // context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor((editor) => {
     //     if (editor && editor.document.languageId === 'b4x') {
@@ -39,96 +41,14 @@ export function deactivate()
     console.log('扩展已停用！');
 }
 
-// function CreateGlobalReferenceTableForDocument(context : vscode.ExtensionContext, document: vscode.TextDocument): boolean
-// {
-//     let globalReference: KeywordInfo[] = [];
-//     let docType: ModuleType = ModuleType.Undefined;
-//     const docName: string = document.fileName;
-
-//     // get the DesignerText and extract document info out
-//     const documentSection: string[] = document.getText().split('@EndOfDesignText@')
-//     if (documentSection.length < 2) {console.log('no DesignerText found'); return false;}
-//     const designText: String = documentSection[0]
-//     const designTextLineCnt: number = designText.split('\n').length
-//     const typeMatchGroup = designText.match('(?<=Type) *= *(\\w+)(?=\\n)')
-//     if (!typeMatchGroup || typeMatchGroup.length < 2) {console.log('no ModuleType found'); return false;}
-//     else 
-//     {
-//         switch (typeMatchGroup[1].toLocaleLowerCase())
-//         {
-//             case 'class':
-//                 docType = ModuleType.Class; break;
-//             case 'staticcode':
-//                 docType = ModuleType.StaticCode; break;
-//             case 'service':
-//                 docType = ModuleType.Service; break;
-//             default:
-//         }
-//     }
-
-//     let globalSubStarted: boolean = false;
-//     let globalSubEnded: boolean = false;
-//     // go through each line of the document to create global reference for Variables and Functions
-//     for (let line: number = designTextLineCnt; line < document.lineCount; line++) 
-//     {
-//         const text: string = document.lineAt(line).text
-//         const lowerCaseText: String = text.toLowerCase();
-//         // cheching if this line is a comment line, if it is ignore
-//         if (lowerCaseText.trim().startsWith("'")) {continue;}
-
-//         if (!globalSubEnded)
-//         {
-//             if (globalSubStarted)
-//             {
-//                 // inside global sub, all global variables are declared here
-//                 const variableMatchPattern: string = `${comRegExp.StartOfWord}(\\w+) +As +(\\w+)${comRegExp.EndOfWord}`;
-//                 const variableMatchResult = lowerCaseText.match(new RegExp(variableMatchPattern, comRegExp.Flag.CaseIncensitive));
-//                 if (variableMatchResult && variableMatchResult.length > 2)
-//                 {
-//                     //find a global variable
-//                     const globalVariableToAdd: KeywordInfo = {
-//                         KeywordName: variableMatchResult[1],
-//                     ClassName: variableMatchResult[2],
-//                     ModuleName: docName,
-//                     ModuleType: docType,
-//                     Scope: KeywordScope.Global,
-//                     DefinitionPos: new vscode.Position(line, 0),
-//                     //Type
-//                     }
-//                 }
-//                 const isGlobalSubEndFound: boolean = lowerCaseText.trim().includes("End Sub".toLowerCase());
-//                 globalSubEnded = isGlobalSubEndFound;
-//             } else
-//             {
-//                 const isGlobalSubFound: boolean = lowerCaseText.trim().includes("Sub Class_Globals".toLowerCase()) || 
-//                                                   lowerCaseText.trim().includes("Sub Process_Globals".toLowerCase());
-//                 globalSubStarted = isGlobalSubFound;
-//             }
-//         } else
-//         {
-        
-//             //     // 检查是否包含目标单词
-//             //     const isEventFound: boolean = text.includes(`Sub ${word}_`.toLowerCase());
-//             //     if (isEventFound) {continue;}
-                
-//             //     const functionMatchPattern: string = `Sub ${word}${comRegExp.EndOfWord}`;
-//             //     const functionMatchResult = lowerCaseText.match(new RegExp(functionMatchPattern, comRegExp.Flag.CaseIncensitive));
-        
-//             //     const variableMatchPattern: string = `${comRegExp.StartOfWord}${word} As`;
-//             //     const variableMatchResult = lowerCaseText.match(new RegExp(variableMatchPattern, comRegExp.Flag.CaseIncensitive));
-//             //     //const isVariableFound: boolean = lowerCaseText.includes(`${word} As`.toLowerCase());
-//             //     if (functionMatchResult || variableMatchResult) 
-//             //     {
-//             //         // 返回定义的位置
-//             //         retWordInfo.DefinitionPos = new vscode.Position(line, text.indexOf(word));
-//             //         retWordInfo.Scope = KeywordScope.Global;
-//             //         if (functionMatchResult){retWordInfo.Type = KeywordType.Sub;}
-//             //         if (variableMatchResult){retWordInfo.Type = KeywordType.Variable;}
-//             //         return retWordInfo;
-//             //     }
-//         }
-
-
-//     }
-//     return true
-// }
+class b4xSignatureHelpProvider implements vscode.SignatureHelpProvider
+{
+    provideSignatureHelp(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.SignatureHelpContext): vscode.ProviderResult<vscode.SignatureHelp> 
+    {
+        let retVal = new vscode.SignatureHelp();
+        const signatureInfo = new vscode.SignatureInformation('Haha', 'HahaInfoa');
+        retVal.signatures = [signatureInfo];
+        retVal.activeParameter = 1;
+        return retVal;
+    }
+}
