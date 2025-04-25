7eda5db4545aeeded3b32559d20d902df0ffd339
Jansen <25320513+Jansen611@users.noreply.github.com>
2025-04-25 17:56:44 +1000

    Adding Hover Support for b4x baseclass members

--- src/b4xHoverProvider.ts
+++ src/b4xHoverProvider.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 import * as b4xDefinitionProvider from './b4xDefinitionProvider';
 import * as docMethods from './documentMethods';
+import * as b4xBaseClassInfo from './b4xBaseClassInfo'
 
 export class b4xHoverProvider implements vscode.HoverProvider 
 {
@@ -13,40 +14,55 @@ export class b4xHoverProvider implements vscode.HoverProvider
 
         if (word) 
         {
-            const definitionInfo = b4xDefinitionProvider.findDefinitionPosition(document, word, wordLineNo);
-            let declaration: string | undefined = b4xDefinitionProvider.getDeclarationStringFromSearch(document, word, wordLineNo);                
-            if (declaration) 
+            let declaration: string = ""
+            // check whether this is a member search or global search
+            const parentObjectMatch = docMethods.getAllParentObjMatchFromDocPosition(document, position); // looking for '.' operator
+            if (parentObjectMatch && parentObjectMatch.length > 0)
             {
-                if (definitionInfo.Type == b4xDefinitionProvider.KeywordType.Variable)
+                // loop through all matches to find in which class space to do the search
+                for (let i:number = 0; i < parentObjectMatch.length; i++)
                 {
-                    if (definitionInfo.Scope == b4xDefinitionProvider.KeywordScope.Global)
+                    const keywordMatch = parentObjectMatch[i].match('\\w+');
+                    if (keywordMatch)
                     {
-                        declaration = "(global variable) " + declaration;
-                    } else if (definitionInfo.Scope == b4xDefinitionProvider.KeywordScope.Local)
-                    {
-                        declaration = "(local variable) " + declaration;
+                        let outKeywordInfo = b4xDefinitionProvider.findDefinitionPosition(document, keywordMatch[0], position.line);
+                        if (outKeywordInfo.ClassName && b4xBaseClassInfo.B4X_BASECLASS_MEMBER_COMPLETION[outKeywordInfo.ClassName.toLowerCase()])
+                        {
+                            // this is a member inside b4x base class
+                            declaration = b4xBaseClassInfo.B4X_BASECLASS_MEMBER_DECLARATION[`${outKeywordInfo.ClassName.toLowerCase()}.${word.toLowerCase()}`];
+                            if (declaration)
+                            {
+                                break;
+                            }
+                        }
                     }
-                } else if (definitionInfo.Type == b4xDefinitionProvider.KeywordType.Parameter)
-                {
-                    declaration = "(parameter) " + declaration;
                 }
+            }
 
-                // if (definitionInfo.Scope == b4xDefinitionProvider.KeywordScope.Global &&
-                //     definitionInfo.Type == b4xDefinitionProvider.KeywordType.Variable)
-                // {
-                //     switch(definitionInfo.ModuleType)
-                //     {
-                //         case b4xDefinitionProvider.ModuleType.Class:
-                //         case b4xDefinitionProvider.ModuleType.StaticCode:
-                //             declaration = "(global variable) " + declaration;
-                //             break;
-                //         // case b4xDefinitionProvider.ModuleType.StaticCode:
-                //         //     declaration = "(process_globals) " + declaration;
-                //         //     break;
-                //         default:
-                //     }
-                // }
+            if (!declaration)
+            {
+                const definitionInfo = b4xDefinitionProvider.findDefinitionPosition(document, word, wordLineNo);
+                declaration = b4xDefinitionProvider.getDeclarationStringFromSearch(document, word, wordLineNo) || "";                
+                if (declaration) 
+                {
+                    if (definitionInfo.Type == b4xDefinitionProvider.KeywordType.Variable)
+                    {
+                        if (definitionInfo.Scope == b4xDefinitionProvider.KeywordScope.Global)
+                        {
+                            declaration = "(global variable) " + declaration;
+                        } else if (definitionInfo.Scope == b4xDefinitionProvider.KeywordScope.Local)
+                        {
+                            declaration = "(local variable) " + declaration;
+                        }
+                    } else if (definitionInfo.Type == b4xDefinitionProvider.KeywordType.Parameter)
+                    {
+                        declaration = "(parameter) " + declaration;
+                    }
+                }
+            }
 
+            if (declaration)
+            {
                 // Create a MarkdownString to format the hover content
                 const markdownString = new vscode.MarkdownString();
                 // Use 'b4x' as the language identifier for syntax highlighting
@@ -55,6 +71,8 @@ export class b4xHoverProvider implements vscode.HoverProvider
                 // return hover info
                 return new vscode.Hover(markdownString);
             }
+
+
         }
 
         // no definition found, undefined
